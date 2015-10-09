d79b9c4eced5fe60bd15b806b4a28c8f39d94f02
ndousse <nicolas.dousse@epfl.ch>
2015-10-09 16:53:56 +0200

    Change number of volumes to enable MMC disks

--- src/ffconf.h
+++ src/ffconf.h
@@ -141,7 +141,7 @@
 / Drive/Volume Configurations
 /---------------------------------------------------------------------------*/
 
-#define _VOLUMES	1
+#define _VOLUMES	3
 /* Number of volumes (logical drives) to be used. */
 
 
